f4d34f6224626dad6b7c5289774b642b1e493ee2
Andre Martov <andremartov@users.noreply.github.com>
2017-09-26 14:19:06 +0300

    uued asjad

---
+++ tund3/avaleht.php
@@ -0,0 +1,20 @@
+<?php
+	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Andre Martov veebiprogrammeerimine</title>
+</head>
+<body>
+	<h1>Avaleht</h1>
+	<p>See leht on loodud õppetöö raames ning ei sisalda mingit tõsiseltvõetavat sisu.</p>
+	<p>Meie õpime <a href="http://www.tlu.ee">Tallinna Ülikoolis</a>.</p>
+	<p>Minu esimene PHP leht asub <a href="../esimene.php">siin</a>.</p>
+	<p>Minu sõbra Tim'i leht asub <a href="../../../../~jaantim/veebiproge">siin</a>.</p>
+	<p>Pilte näeb <a href="foto.php">siin</a>.</p>
+	
+</body>
+</html>
